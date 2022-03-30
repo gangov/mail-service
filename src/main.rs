6d342ce4eab954c7def593b4f9942171e970af1b
Kaloyan Gangov <gangov1@gmail.com>
2022-03-30 22:52:21 +0300

    chapter #3.7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use mail_service::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  let listener = TcpListener::bind("127.0.0.1:8000").expect("Can't bind");
+  run(listener)?.await
 }
